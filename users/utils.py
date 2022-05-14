69cfff47ab8902d2dc6dd012924048d385f78909
mmaslan <marek.maslany1@gmail.com>
2022-05-14 13:57:35 +0200

    Two new functions

--- users/utils.py
+++ users/utils.py
@@ -3,3 +3,7 @@ import hashlib
 def hash_password_without_salt(password: str ) -> str:
     hash_password = hashlib.sha256(password.encode("ASCII")).hexdigest()
     return hash_password
+
+def verification_without_salt(password: str, hash_password: str) -> bool:
+    input_password = hashlib.sha256(password.encode("ASCII")).hexdigest()
+    return input_password == hash_password
