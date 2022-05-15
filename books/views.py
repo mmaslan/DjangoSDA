ecc978835ba520d143ff3f1a90f952c74e55efd4
mmaslan <marek.maslany1@gmail.com>
2022-05-15 11:13:21 +0200

    Redirect home to login

--- books/views.py
+++ books/views.py
@@ -1,9 +1,9 @@
 from uuid import uuid4
 
-from django.core.exceptions import BadRequest
+from django.core.exceptions import BadRequest, PermissionDenied
 from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse, JsonResponse
-from django.urls import reverse_lazy
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, get_object_or_404
@@ -119,8 +119,16 @@ class BookDeleteView(DeleteView):
 # 11.
 
 def get_hello(request: WSGIRequest) -> HttpResponse:
-    hello = "Hello world!"
-    return render(request, template_name="Hello_world.html", context={"hello_var": hello})
+    user: User = request.user  # type: ignore
+    # password = None if user.is_anonymous else user.password
+    # email = None if user.is_anonymous else user.email
+    # date_joined = None if user.is_anonymous else user.date_joined
+    if not user.is_authenticated:
+        # raise PermissionDenied()
+        return HttpResponseRedirect(reverse('login'))
+    is_auth: bool = user.is_authenticated
+    hello = f"Hello {user.username}. Your password: {user.password}, your email: {user.email} and date you joined: {user.date_joined}!"
+    return render(request, template_name="Hello_world.html", context={"hello_var": hello, "is_authenticated": is_auth})
 
 # 12.
 
